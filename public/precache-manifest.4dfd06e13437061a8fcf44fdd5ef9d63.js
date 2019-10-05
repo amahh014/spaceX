536187d2a5e29b0f29a72489e149526003779e82
amahh014 <nizammamedovv@gmail.com>
2019-10-05 21:45:28 +0200

    Finished Project

---
+++ public/precache-manifest.4dfd06e13437061a8fcf44fdd5ef9d63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763ffa4ece9139064c721878f331ddb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec9a3f224b9374e9be51",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "c8c46f88ebcda18ef1a0",
+    "url": "/static/js/2.be29180b.chunk.js"
+  },
+  {
+    "revision": "ec9a3f224b9374e9be51",
+    "url": "/static/js/main.d3608a3a.chunk.js"
+  },
+  {
+    "revision": "21cee84d87d5e9d90778",
+    "url": "/static/js/runtime-main.37e76046.js"
+  },
+  {
+    "revision": "6b7cbac7b67058e2ee065f596909093c",
+    "url": "/static/media/logo.6b7cbac7.jpg"
+  }
+]);
